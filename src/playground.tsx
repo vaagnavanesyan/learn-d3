522031ea96089fe48f8dc9f288b3aa26f90b8bcd
Vaagn Avanesyan <vahagn1993@gmail.com>
2022-06-11 16:11:46 +0300

    feat(*): add d3 playground

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,29 +1,61 @@
 import { useEffect, useRef } from "react";
-import { renderToString } from "react-dom/server";
+import * as d3 from "d3";
+
+// const applyAttrs = (element: any, attributes: Record<string, any>) => {
+//   Object.entries(attributes).forEach(([key, value]) => {
+//     element.attr(key, value);
+//   });
+// };
+// applyAttrs(svg, {
+//   width: 600,
+//   height: 600,
+// });
 
 export const Playground = () => {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (!ref.current) return;
-    ref.current.innerHTML = renderToString(
-      <svg width={600} height={600} style={{ border: "2px solid black" }}>
-        <path
-          d='M 150 50 L 75 200 L 225 200 C 225 200 150 150 150 50 '
-          fill='orange'
-          strokeWidth={4}
-        />
-        <circle cx={150} cy={150} r={5} fill='grey' />
-        <line
-          x1={225}
-          y1={200}
-          x2={150}
-          y2={150}
-          stroke='grey'
-          strokeWidth={2}
-        />
-      </svg>
-    );
+    const svg = d3.create("svg").attr("width", 600).attr("height", 600);
+    const group = svg.append('g')
+    .attr('transform', 'translate(50, 100)')
+
+    group
+      .append("rect")
+      .attr("width", 200)
+      .attr("height", 100)
+      .attr("fill", "blue")
+      .attr("x", 20)
+      .attr("y", 20);
+
+      group
+      .append("circle")
+      .attr("r", 50)
+      .attr("cx", 300)
+      .attr("cy", 70)
+      .attr("fill", "pink");
+
+      group
+      .append("line")
+      .attr("x1", 370)
+      .attr("x2", 400)
+      .attr("y1", 20)
+      .attr("y2", 120)
+      .attr('stroke', 'red');
+
+      group
+      .append("text")
+      .attr('x', 20)
+      .attr('y', 200)
+      .attr('fill', 'grey')
+      .text('hello')
+      .style('font-family', 'courier')
+      .style('font-weight', 'bold')
+
+
+
+
+    ref.current.innerHTML = svg.node()?.outerHTML ?? "empty...";
   }, []);
 
   return <div ref={ref}></div>;
