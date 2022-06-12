58517403152cdf2e32dfc6c3a73ede12c45b77b4
Vaagn Avanesyan <vahagn1993@gmail.com>
2022-06-13 00:02:49 +0300

    feat(*): draw donut with labels

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,59 +1,64 @@
+// https://dev.to/vineethtrv/react-d3-donut-chart-49cm
 import { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-// const applyAttrs = (element: any, attributes: Record<string, any>) => {
-//   Object.entries(attributes).forEach(([key, value]) => {
-//     element.attr(key, value);
-//   });
-// };
-// applyAttrs(svg, {
-//   width: 600,
-//   height: 600,
-// });
+type Slice = {
+  name: string;
+  cost: number;
+  color: string;
+};
 
 export const Playground = () => {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (!ref.current) return;
-    const svg = d3.create("svg").attr("width", 600).attr("height", 600);
-    const group = svg.append('g')
-    .attr('transform', 'translate(50, 100)')
+
+    const data = [
+      { name: "Money", cost: 86.66, color: "#ef3024" },
+      { name: "Bonds", cost: 12.7, color: "#ffcb03" },
+      { name: "Stocks", cost: 0.64, color: "#13a463" },
+    ];
+
+    const svg = d3.create("svg").attr("width", 240).attr("height", 240);
+    const group = svg.append("g").attr("transform", "translate(120, 120)");
+
+    const pie = d3.pie<Slice>().value(({ cost }) => cost);
+
+    const slicesArc = d3
+      .arc<d3.PieArcDatum<Slice>>()
+      .outerRadius((_, i) => 120 - i * 5)
+      .innerRadius(45)
+      .padAngle(Math.PI / 180);
+
+    const textArc = d3
+      .arc<d3.PieArcDatum<Slice>>()
+      .startAngle((d) => d.startAngle - Math.PI / 5)
+      .endAngle((d) => d.endAngle)
+      .outerRadius(125)
+      .innerRadius(25);
 
     group
-      .append("rect")
-      .attr("width", 200)
-      .attr("height", 100)
-      .attr("fill", "blue")
-      .attr("x", 20)
-      .attr("y", 20);
+      .selectAll("path")
+      .data(pie(data))
+      .enter()
+      .append("path")
+      .attr("d", slicesArc)
+      .attr("fill", ({ data: { color } }) => color);
 
-      group
-      .append("circle")
-      .attr("r", 50)
-      .attr("cx", 300)
-      .attr("cy", 70)
-      .attr("fill", "pink");
-
-      group
-      .append("line")
-      .attr("x1", 370)
-      .attr("x2", 400)
-      .attr("y1", 20)
-      .attr("y2", 120)
-      .attr('stroke', 'red');
-
-      group
+    group
+      .selectAll("text")
+      .data(data)
+      .enter()
       .append("text")
-      .attr('x', 20)
-      .attr('y', 200)
-      .attr('fill', 'grey')
-      .text('hello')
-      .style('font-family', 'courier')
-      .style('font-weight', 'bold')
-
-
-
+      .attr(
+        "transform",
+        (d, i) => `translate(${textArc.centroid(pie(data)[i])})`
+      )
+      .text((d) => (d.cost > 1 ? `${d.cost}%` : ""))
+      .attr('font-size', '12px')
+      .attr('font-family', 'Trebuchet MS')
+      .attr('fill', 'white')
 
     ref.current.innerHTML = svg.node()?.outerHTML ?? "empty...";
   }, []);
